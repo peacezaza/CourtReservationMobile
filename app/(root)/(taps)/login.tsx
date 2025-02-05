fbae9ada241386d3f69b247b258b7ee2b3abfb5a
rungwikrai <oakkmmmkk22@gmail.com>
2025-02-05 20:04:41 +0700

    add file login

--- app/(root)/(taps)/login.tsx
+++ app/(root)/(taps)/login.tsx
@@ -1,10 +1,13 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
     <View style={myStyleSheet.container}>
       <Text style={myStyleSheet.text_login}>Log in</Text>
+      <TextInput style={myStyleSheet.textinput} placeholder='Username' keyboardType='default'></TextInput>
+      <TextInput style={myStyleSheet.textinput} placeholder='Password' keyboardType='default'></TextInput>
+      <Button title='Log in' color={"black"} />
     </View>
   )
 }
@@ -18,7 +21,19 @@ const myStyleSheet = StyleSheet.create({
         alignItems:"center"
     },
     text_login:{
-        fontSize:50,
-        fontWeight:"bold"
+        fontSize:40,
+        fontWeight:"bold",
+        margin:40,
+    },
+    textinput:{
+        borderColor:"#0002",
+        borderWidth:1,
+        borderRadius:10,
+        margin : 10,
+        padding:10,
+        width:250
+    },
+    buttoninput:{
+        color:"black"
     }
 })
